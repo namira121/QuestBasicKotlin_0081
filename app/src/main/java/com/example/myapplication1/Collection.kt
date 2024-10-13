8f40177b78ddfc2c6659c43aa7131e56f668859a
namira121 <127211008+namira121@users.noreply.github.com>
2024-10-13 16:50:12 +0700

    Menambahkan data ke dalam map mutable shape dengan menambahkan Rectangle sebagai data string dan 4 untuk data integer

--- app/src/main/java/com/example/myapplication1/Collection.kt
+++ app/src/main/java/com/example/myapplication1/Collection.kt
@@ -80,6 +80,10 @@ println()
     //Map Mutable
     val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(shape)
+
+    //Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
 }
 
 fun main(){
