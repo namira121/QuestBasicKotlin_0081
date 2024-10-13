cb94480062db132474f6a410e0d41cad590f150c
namira121 <127211008+namira121@users.noreply.github.com>
2024-10-13 17:06:13 +0700

    membuat fungsi ConditionalStatement dengan kata kunci fun

---
+++ app/src/main/java/com/example/myapplication1/ConditionalExpression.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication1
+
+// Conditional Expression
+// Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatement(){
+
+}
