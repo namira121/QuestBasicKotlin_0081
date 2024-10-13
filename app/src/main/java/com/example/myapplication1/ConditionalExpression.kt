69e989a09c7ab8610609dd833ebd935273e3c6b5
namira121 <127211008+namira121@users.noreply.github.com>
2024-10-13 17:09:54 +0700

    membuat kondisi if else jika number lebih dari 10 akan muncul pernyataan positive number, jika tidak akan muncul pernyataan negative number

--- app/src/main/java/com/example/myapplication1/ConditionalExpression.kt
+++ app/src/main/java/com/example/myapplication1/ConditionalExpression.kt
@@ -9,5 +9,10 @@ println("=== Conditional Expression ===")
 
     //If-Else
     val number = 10
+    if (number > 0){
+        println("Positive Number")
+    }else{
+        println("Negative Number")
+    }
 
 }
