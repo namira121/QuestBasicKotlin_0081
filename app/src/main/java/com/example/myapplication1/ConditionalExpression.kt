2e23946c9515178f8969ed5ae2254b83fc2562a4
namira121 <127211008+namira121@users.noreply.github.com>
2024-10-13 17:15:57 +0700

    membuat kondisi when ketika nilai day dimasukkan akan menampilkan pernyataan sesuai dengan nilai day. Jika nilai tidak sesusai akan menampilkan pesan invalid

--- app/src/main/java/com/example/myapplication1/ConditionalExpression.kt
+++ app/src/main/java/com/example/myapplication1/ConditionalExpression.kt
@@ -17,5 +17,15 @@ println("=== Conditional Expression ===")
 
     //When
     val day = 1
+    when(day){
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+    }
 
 }
